34df5ebddea3fa995a5297ba7963f9f74b04d1a1
itsunknown01 <aryamangohain@gmail.com>
2023-11-01 02:58:06 +0530

    Completed Hone Page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,32 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Header from '@/components/layouts/Header'
 
-const inter = Inter({ subsets: ['latin'] })
+import Footer from "@/components/layouts/footer";
+import Header from "@/components/layouts/header";
+import { QueryProvider } from "@/components/providers/query-provider";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'LocalBazaar',
-  description: 'Created by Ayushman Gohain',
-}
+  title: "LocalBazaar",
+  description: "Created by Ayushman Gohain",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {children}
+      <QueryProvider>
+          <Header />
+          {children}
+          <Footer />
+      </QueryProvider>
       </body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
-import Image from 'next/image'
+import FamousSection from "@/components/layouts/famous-section";
+import FeatureSection from "@/components/layouts/feature-section";
+import HeroSection from "@/components/layouts/hero-section";
+import PopularSection from "@/components/layouts/popular-section";
+import SpecialProduct from "@/components/layouts/special-product";
+import Trusted from "@/components/layouts/trusted";
 
 export default function Home() {
+
   return (
-    <main className='flex flex-col w-full h-full'>
-      
+    <main className="flex flex-col w-full h-full">
+      <HeroSection />
+      <FeatureSection />
+      <FamousSection />
+      <Trusted />
+      <SpecialProduct />
+      <PopularSection />
     </main>
-  )
+  );
 }
